6e618c1c6f2bdf0702704955e3f29bcbe0314341
Chris Dzombak <chris@chrisdzombak.net>
2019-09-04 10:59:18 -0400

    assimilate script uses cecho

--- script/assimilate.sh
+++ script/assimilate.sh
@@ -2,6 +2,12 @@
 
 set -uo pipefail
 IFS=$'\n\t'
+source ./lib/cecho
+
+if [ "$(uname)" != "Darwin" ]; then
+  cecho "Skipping osx-automation assimilation because not on macOS" $red
+  exit 2
+fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$SCRIPT_DIR/.." || exit 1
@@ -13,7 +19,7 @@ DIRS=(
   "opt"
 )
 
-command -v ksdiff >/dev/null || (echo "Requires Kaliedoscope & ksdiff CLI tool to be installed."; exit 2)
+command -v ksdiff >/dev/null || (cecho "Requires Kaliedoscope & ksdiff CLI tool to be installed." $red; exit 1)
 command -v fork && fork
 
 for DIR in "${DIRS[@]}"; do
